5ee233ebcede7bc3957b6d1d7e6c94ecbe6a4966
cbrianbet <cbrianbet@outlook.com>
2022-09-01 18:09:17 +0300

    added hydrate to manifests

--- src/application/stage/commands/handlers/sync.handler.ts
+++ src/application/stage/commands/handlers/sync.handler.ts
@@ -12,18 +12,76 @@ import { Logger } from '@nestjs/common';
 import { UpdateStatusCommand } from '../update-status.command';
 import { ConfigService } from '../../../../config/config.service';
 import { MessagingService } from '../../../../infrastructure/messging/messaging.service';
+import { Manifest } from './../../../../domain/manifest.entity';
 
 @CommandHandler(SyncCommand)
 export class SyncHandler implements ICommandHandler<SyncCommand> {
   constructor(
     @InjectRepository(Stats)
     private readonly repository: Repository<Stats>,
+    @InjectRepository(Manifest)
+    private readonly manifestRepository: Repository<Manifest>,
     private readonly commandBus: CommandBus,
     private readonly config: ConfigService,
     private readonly client: MessagingService,
   ) {}
 
   async execute(command: SyncCommand): Promise<any> {
+    const manifests = await this.manifestRepository
+      .createQueryBuilder('s')
+      .where('s.facilityCode = :facilityCode', {
+        facilityCode: command.manifet.facilityCode,
+      })
+      .andWhere('s.docket like :docket', {
+        docket: '%' + command.manifet.docket + '%',
+      })
+      .orderBy({
+        's.statusDate': 'DESC',
+      }).getMany()
+      // .getOne();
+
+    if (manifests) {
+      manifests.forEach(async m => {
+        try {
+          const manifestResult = await this.client.publish(
+            JSON.stringify(m),
+            this.config.QueueStatsExchange,
+            this.config.getRoute(Manifest.name.toLowerCase()),
+          );
+          Logger.log('PUBLISHED MANIFESTS');
+          
+          if (manifestResult) {
+            await this.commandBus.execute(
+              new UpdateStatusCommand(
+                m.id,
+                Manifest.name,
+                'SENT',
+              ),
+            );
+          } else {
+            await this.commandBus.execute(
+              new UpdateStatusCommand(
+                m.id,
+                Stats.name,
+                'ERROR',
+                'Unkown publish Error',
+              ),
+            );
+          }
+          
+
+        } catch (e) {
+          Logger.error(e);
+          await this.commandBus.execute(
+            new UpdateStatusCommand(m.id, Stats.name, 'ERROR', e),
+          );
+        }
+      });
+    }
+
+
+
+    
     const stats = await this.repository
       .createQueryBuilder('s')
       .where('s.facilityCode = :facilityCode', {
@@ -47,7 +105,7 @@ export class SyncHandler implements ICommandHandler<SyncCommand> {
           this.config.QueueStatsExchange,
           this.config.getRoute(Stats.name.toLowerCase()),
         );
-        Logger.log('PUBLISHED');
+        Logger.log('PUBLISHED STATS');
 
         if (result) {
           await this.commandBus.execute(
